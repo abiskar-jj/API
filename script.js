914d1e56c36187262451fb16bc320424aa58cf7e
abiskar-jj <abiskarchhetri86@gmail.com>
2025-12-05 20:55:12 -0500

    button click event and basic fetch added

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ const apiKey = "e59e000761da2e0447d96d54df8bdc28";
 
 const button = document.getElementById("getWeatherBtn");
 const resultBox = document.getElementById("weatherResult");
+
+button.addEventListener("click", function () {
+    
+    const city = document.getElementById("cityInput").value;
+
+    if (city === "") {
+        resultBox.innerHTML = "Please type a city name.";
+        return;
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            resultBox.innerHTML = "API call successful.";
+        });
+});
