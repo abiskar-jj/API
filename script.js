1ab6dcb1da4620fdc0066bcf5be0e846d8d63d0f
abiskar-jj <abiskarchhetri86@gmail.com>
2025-12-05 20:46:54 -0500

    basic variable setup

---
+++ script.js
@@ -0,0 +1,5 @@
+// OpenWeatherMap API key
+const apiKey = "e59e000761da2e0447d96d54df8bdc28";
+
+const button = document.getElementById("getWeatherBtn");
+const resultBox = document.getElementById("weatherResult");
